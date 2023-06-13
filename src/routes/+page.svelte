d19323fb1e113d33f0560829b09bc1b5c8ff80ba
IThundxr <contact@ithundxr.dev>
2023-06-13 19:54:22 -0400

    ✨Add some basic structure✨
    Signed-off-by: IThundxr <contact@ithundxr.dev>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,32 +5,42 @@
 	<title>MMPA</title>
 </svelte:head>
 
-<main class="text-white flex flex-row justify-center w-full h-full bg-gradient-to-bl from-black to-[#0d131f] min-h-screen">
-
-<div class="fixed top-0 right-0 h-11 w-full bg-[#0d131f] z-50">
-	<div class="flex justify-end list-reset">
-		<div class="fixed top-0 m-1 rounded-full left-2 h-9 w-9 drop-shadow-lg">
+<main class="flex flex-row justify-center w-full h-full min-h-screen text-white bg-primary">
+	<!-- Header START -->
+	<div class="fixed top-0 right-0 z-50 w-full h-11">
+		<div class="fixed m-1 rounded-full top-1 left-2 h-9 w-9 drop-shadow-lg">
 			<img
-				src="/fmt-logo-chroma+transparent.png"
+				src="/fmt-logo-chroma+glow.png"
 				class="rounded-full"
 				alt="MMPA Logo"
 				height="36"
 				width="36"
 			/>
-			<span class="text-1xl relative m-2 left-[50px] bottom-[30px] font-medium"
-				>MMPA</span
-			>
+			<span class="text-1xl relative m-2 left-[40px] bottom-[30px] font-medium">MMPA</span>
 		</div>
 	</div>
-</div>
+	<!-- Header END -->
 
-
-<div class="w-screen h-12 bottom-0 flex fixed justify-center items-center bg-[#0d131f] z-50">
-
-	<div class="m-auto flex-nowrap">
-		<span class="text-xs">This Site is made with Open Source Software</span>
-		<span class="inline-block text-xs">© 2023 Minecraft Malware Prevention Alliance</span>
+	<!-- Middle START -->
+	<div class="flex items-center justify-center w-screen h-screen">
+		<div class="p-10 space-y-5 rounded-2xl">
+			<h1 class="text-3xl text-center">Minecraft Malware Prevention Alliance</h1>
+			<p class="text-center text-1xl">
+				The Minecraft Malware Prevention Alliance or MMPA for short is a group of people who are
+				<br />
+				dedicated to preventing malware from spreading in the Minecraft community.
+			</p>
+		</div>
 	</div>
-</div>
+	<!-- Middle END -->
 
+	<!-- Footer START -->
+	<div class="fixed bottom-0 z-50 flex items-center justify-center w-screen h-6">
+		<div class="flex flex-col items-center m-auto">
+			<!-- <span class="text-xs">This Site is fully Open Source</span> -->
+			<span class="text-xs">© 2023 Minecraft Malware Prevention Alliance</span>
+		</div>
+		<span class="text-primary">uwu</span>
+	</div>
+	<!-- Footer END -->
 </main>
