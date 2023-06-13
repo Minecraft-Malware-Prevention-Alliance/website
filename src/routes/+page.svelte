c7e87ad0b8f1941ec6271b1e524b1d8cb80f14b6
Harsh Dhaliwal <contact@ithundxr.dev>
2023-06-12 21:06:51 -0400

    add some structure
    Signed-off-by: Harsh Dhaliwal <contact@ithundxr.dev>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<svelte:head>
+	<title>MMPA</title>
+</svelte:head>
+
+<main class="text-white flex flex-row justify-center w-full h-full bg-gradient-to-bl from-black to-[#0d131f] min-h-screen">
+
+<div class="fixed top-0 right-0 h-11 w-full bg-[#0d131f] z-50">
+	<div class="flex justify-end list-reset">
+		<div class="fixed top-0 m-1 rounded-full left-2 h-9 w-9 drop-shadow-lg">
+			<img
+				src="/fmt-logo-chroma+transparent.png"
+				class="rounded-full"
+				alt="MMPA Logo"
+				height="36"
+				width="36"
+			/>
+			<span class="text-1xl relative m-2 left-[50px] bottom-[30px] font-medium"
+				>MMPA</span
+			>
+		</div>
+	</div>
+</div>
+
+
+<div class="w-screen h-12 bottom-0 flex fixed justify-center items-center bg-[#0d131f] z-50">
+
+	<div class="m-auto flex-nowrap">
+		<span class="text-xs">This Site is made with Open Source Software</span>
+		<span class="inline-block text-xs">© 2023 Minecraft Malware Prevention Alliance</span>
+	</div>
+</div>
+
+</main>
