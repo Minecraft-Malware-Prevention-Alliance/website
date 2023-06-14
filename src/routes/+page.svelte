cf49139148d4060a6f175c6fc6592fa1bea1e7b5
IThundxr <contact@ithundxr.dev>
2023-06-13 21:38:56 -0400

    ✨ Clean up some css ✨
    Signed-off-by: IThundxr <contact@ithundxr.dev>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,25 +5,19 @@
 	<title>MMPA</title>
 </svelte:head>
 
-<main class="flex flex-row justify-center w-full h-full min-h-screen text-white bg-primary">
+<main class="flex flex-row text-white bg-primary">
 	<!-- Header START -->
-	<div class="fixed top-0 right-0 z-50 w-full h-11">
-		<div class="fixed m-3 rounded-full h-9 w-9">
-			<img
-				src="/fmt-logo-chroma+glow.png"
-				class="rounded-full"
-				alt="MMPA Logo"
-				height="40"
-				width="40"
-			/>
-			<span class="text-1xl relative m-2 left-[40px] bottom-[30px] font-medium">MMPA</span>
+	<div class="fixed top-0 left-0">
+		<div class="flex flex-row m-3 rounded-full">
+			<img src="/fmt-logo-chroma+glow.png" class="rounded-full" alt="MMPA Logo" height="40" width="40" />
+			<span class="m-2 font-medium text-1xl">MMPA</span>
 		</div>
 	</div>
 	<!-- Header END -->
 
 	<!-- Middle START -->
 	<div class="flex flex-col items-center justify-center w-screen h-screen">
-		<div class="p-10 space-y-5 rounded-2xl">
+		<div class="mb-10 space-y-5 rounded-2xl">
 			<h1 class="text-3xl text-center">Minecraft Malware Prevention Alliance</h1>
 			<p class="text-center text-1xl">
 				The Minecraft Malware Prevention Alliance or MMPA for short is a group of people who are
@@ -38,19 +32,15 @@
 					<path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"/>
 				</svg>
 				<span class="pl-1">GitHub</span>
-			  </a>
-			  
+			</a>
 		<div/>
 	</div>
 	<!-- Middle END -->
 
 	<!-- Footer START -->
-	<div class="fixed bottom-0 z-50 flex items-center justify-center w-screen h-6 pb-4">
-		<div class="flex flex-col items-center m-auto">
-			<!-- <span class="text-xs">This Site is fully Open Source</span> -->
-			<span class="text-xs">© 2023 Minecraft Malware Prevention Alliance</span>
-		</div>
-		<span class="text-primary">uwu</span>
+	<div class="fixed bottom-0 items-center justify-center mb-3 select-none">
+		<!-- <span class="text-xs">This Site is fully Open Source</span> -->
+		<span class="text-xs">© 2023 Minecraft Malware Prevention Alliance</span>
 	</div>
 	<!-- Footer END -->
 </main>
