17cea0a53ae63c60d556ddfd2aaedd7c546d1a5a
Gareth Coles <gdude2002@gmail.com>
2023-06-15 16:34:05 +0100

    Run linter

--- svelte.config.js
+++ svelte.config.js
@@ -13,12 +13,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-            // See below for an explanation of these options
-            routes: {
-                include: ['/*'],
-                exclude: ['<all>']
-            }
-        })
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
 	}
 };
 
